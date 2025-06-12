aadf4e3d51fb36f6052ffb4631952b8a2e1f3857
suhani11 <suhani_r@me.iitr.ac.in>
2025-06-12 18:12:30 +0530

    Add files via upload

---
+++ ARIES_CHATGPT/rag/retriever.py
@@ -0,0 +1,22 @@
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+import torch
+
+device="cuda" if torch.cuda.is_available() else "cpu"
+
+def load_vector_store():
+    loader = DirectoryLoader("docs", loader_cls=PyPDFLoader)
+    documents = loader.load()
+    splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    docs = splitter.split_documents(documents)
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2",model_kwargs={'device': device})
+    db = FAISS.from_documents(docs, embeddings)
+    return db
+
+vector_db = load_vector_store()
+
+def retrieve_docs(query: str):
+    docs = vector_db.similarity_search(query, k=3)
+    return "\n\n".join([doc.page_content for doc in docs])
